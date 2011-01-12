a5971e023e52e7eb5cf9c31b47ead02c59d422df
bmauser <bmauser@66a88c5f-668a-fc0a-2004-9ab9102d9ccf>
2011-01-12 21:03:17 +0000

    Croatian translation of validation error messages

---
+++ trunk/bvalidator.lang.hr.js
@@ -0,0 +1,27 @@
+var bValidatorOptions = {
+	
+	// croatian error messages
+	errorMessages: {
+		hr: {
+			'default':    'Ispravite ovu vrijednost.',
+			'equalto':    'Unesite ponovno istu vrijednost.',
+			'differs':    'Unesite razlièitu vrijednost.',
+			'minlength':  'Duljina mora biti najmanje {0} znakova.',
+			'maxlength':  'Duljina mora biti najviše {0} znakova.',
+			'rangelength':'Duljina mora biti izmeðu {0} i {1} znakova.',
+			'min':        'Unesite vrijednost veæu ili jednaku {0}.',
+			'max':        'Unesite vrijednost manju ili jednaku {0}.',
+			'between':    'Unesite vrijednost izmeðu {0} i {1}.',
+			'required':   'Ovo polje je obavezno.',
+			'alpha':      'Unesite samo slova.',
+			'alphanum':   'Unesite samo slova i brojeve.',
+			'digit':      'Unesite samo brojeve.',
+			'number':     'Unesite ispravan broj.',
+			'email':      'Unesite ispravnu Email adresu.',
+			'image':      'Odaberite samo slikovne datoteke.',
+			'url':        'Unesite ispravan URL.',
+			'ip4':        'Unesite ispravnu IP adresu.',
+			'date':       'Unesite ispravan datum u formatu {0}.',
+		}
+	}
+};
