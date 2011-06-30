b35fec622bbfa132bf3c67a5c929346d141b598b
bmauser <bmauser@66a88c5f-668a-fc0a-2004-9ab9102d9ccf>
2011-06-30 22:42:04 +0000

    Estonian error messages

---
+++ trunk/bvalidator.lang.ee.js
@@ -0,0 +1,28 @@
+var bValidatorOptions = {
+
+	// Estonian error messages. Thanks to Mare Kotsulim.
+	errorMessages: {
+		ee: {
+			'default':    'Palun paranda see sisestus.',
+			'equalto':    'Palun sisesta sama v‰‰rtus uuesti.',
+			'differs':    'Palun sisesta erinev v‰‰rtus.',
+			'minlength':  'Pikkus peab olema v‰hemalt {0} m‰rki',
+			'maxlength':  'Pikkus voib olla maksimum {0} m‰rki',
+			'rangelength':'Pikkus voib olla vahemikus {0} kuni {1}',
+			'min':        'Palun sisesta number mis on suurem voi vordne {0} ga.',
+			'max':        'Palun sisesta number mis on v‰iksem voi vordne {0} ga.',
+			'between':    'Palun sisesta number vahemikus {0} kuni {1}.',
+			'required':   'Noutud v‰li.',
+			'alpha':      'Lubatud on ainult t‰hed.',
+			'alphanum':   'Lunatud on ainult t‰hed ju numbrid.',
+			'digit':      'Lubatud on ainult numbrid.',
+			'number':     'Palun sisesta korrektne number.',
+			'email':      'Palun sisesta korrektne email.',
+			'image':      'Selles v‰ljas on lubatud ainult pildifailid',
+			'url':        'Palun sisesta korrektne URL.',
+			'ip4':        'Palun sisesta korrektne IPv4 aadress.',
+			'ip6':        'Palun sisesta korrektne IPv6 aadress.',
+			'date':       'Palun sisesta kuup‰ev formaadis {0}.' 
+		}
+	}
+};
